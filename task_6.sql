50dbb9e615f22a36a1d06ee6b491dbe5db550d00
richard-acheampong <richardkaacheampong@gmail.com>
2025-02-02 23:47:57 +0000

    Added additional files

---
+++ task_6.sql
@@ -0,0 +1,7 @@
+USE alx_book_store;
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES [(2, "Blessing Malik", "bmalik@sandtach.com", "124 Happiness Ave."),
+      (3, "Obed Ehoneah", "eobed@sandtach.com", "125 Happiness Ave."),
+      (4, "Nehmial Kamolu", "nkamolu@sandtach.com", "126 Happiness Ave.")
+      ]
